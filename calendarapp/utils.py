5dbfae4f9fc5460b6d7e9820e2c187b66600097b
sajib1066 <sajib1066@gmail.com>
2020-07-09 19:32:31 +0600

    fix show event issue

--- calendarapp/utils.py
+++ calendarapp/utils.py
@@ -34,7 +34,7 @@ class Calendar(HTMLCalendar):
 	# formats a month as a table
 	# filter events by year and month
 	def formatmonth(self, withyear=True):
-		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month, user=get_current_user())
+		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
 
 		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
