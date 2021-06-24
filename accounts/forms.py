9b84a8cfd7403546298a39edd51566530210f172
Sajib Hossain <sajib1066@gmail.com>
2021-06-24 21:31:42 +0600

    add new signin method

---
+++ accounts/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class SignInForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }))
--- accounts/views/signin.py
+++ accounts/views/signin.py
@@ -1,10 +1,32 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+from accounts.forms import SignInForm
 
 
 class SignInView(View):
     """ User registration view """
     template_name = 'accounts/signin.html'
+    form_class = SignInForm
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        forms = self.form_class()
+        context = {
+            'form': forms
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        forms = self.form_class(request.POST)
+        if forms.is_valid():
+            username = forms.cleaned_data['username']
+            password = forms.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('calendarapp:calendar')
+        context = {
+            'form': forms
+        }
+        return render(request, self.template_name, context)
