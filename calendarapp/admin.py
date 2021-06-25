2a9511ac2e7ce64b6895eb6d9c024c12358e2f36
Sajib Hossain <sajib1066@gmail.com>
2021-06-25 17:03:33 +0600

    update event admin

--- calendarapp/admin.py
+++ calendarapp/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from calendarapp.models import Event, EventMember
+from calendarapp import models
 
 
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    model = models.Event
+    list_display = [
+        'id', 'title', 'user', 'is_active', 'is_deleted', 'created_at',
+        'updated_at'
+    ]
+    list_filter = ['is_active', 'is_deleted']
+    search_fields = ['title']
+
+
+@admin.register(models.EventMember)
 class EventMemberAdmin(admin.ModelAdmin):
-    model = EventMember
-    list_display = ['event', 'user']
-
-
-admin.site.register(Event)
-admin.site.register(EventMember, EventMemberAdmin)
+    model = models.EventMember
+    list_display = ['id', 'event', 'user', 'created_at', 'updated_at']
+    list_filter = ['event']
