294cc6a406a5d9af25fd41e05a2e437b1511c179
Sajib Hossain <sajib1066@gmail.com>
2021-06-26 22:16:52 +0600

    add user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+
+from accounts.models import User
 
 
 class SignInForm(forms.Form):
@@ -8,3 +12,43 @@ class SignInForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control'
     }))
+
+
+class SignUpForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control'
+        }),
+        validators=[validate_password]
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control'
+        }),
+        validators=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control'
+            })
+        }
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password didn't match!")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- accounts/views/signup.py
+++ accounts/views/signup.py
@@ -1,10 +1,27 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from accounts.forms import SignUpForm
 
 
 class SignUpView(View):
     """ User registration view """
     template_name = 'accounts/signup.html'
+    form_class = SignUpForm
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        forms = self.form_class()
+        context = {
+            'form': forms
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        forms = self.form_class(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('accounts:signin')
+        context = {
+            'form': forms
+        }
+        return render(request, self.template_name, context)
