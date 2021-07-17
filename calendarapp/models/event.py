caca09822a5c74dda0cb3335f72e17975d06e5a7
Sajib Hossain <sajib1066@gmail.com>
2021-07-17 22:07:30 +0600

    add django doctor

--- calendarapp/models/event.py
+++ calendarapp/models/event.py
@@ -24,7 +24,9 @@ class EventManager(models.Manager):
 
 class Event(EventAbstract):
     """ Event model """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='events'
+    )
     title = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     start_time = models.DateTimeField()
--- calendarapp/models/event_member.py
+++ calendarapp/models/event_member.py
@@ -6,8 +6,12 @@ from calendarapp.models import Event, EventAbstract
 
 class EventMember(EventAbstract):
     """ Event member model """
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name='events'
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='event_members'
+    )
 
     class Meta:
         unique_together = ['event', 'user']
