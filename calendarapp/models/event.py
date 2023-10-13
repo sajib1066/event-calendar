a502fe1a177e32c2efe9718d1189cf32bb118304
lauf8 <lucasafonsodesouza@gmail.com>
2023-10-13 13:46:16 -0300

    feat: add recurring events in weeks and  days

--- calendarapp/models/event.py
+++ calendarapp/models/event.py
@@ -27,7 +27,7 @@ class Event(EventAbstract):
     """ Event model """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
